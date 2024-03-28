9c8ccadb2290f57973978505ae30f194c6645a71
Ngo-viet <ngoviet9898@gmail.com>
2024-03-28 21:47:18 +0700

    code backend

---
+++ src/main/java/com/project/shopapp/controllers/UserController.java
@@ -0,0 +1,171 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.models.Role;
+import com.project.shopapp.models.Token;
+import com.project.shopapp.models.User;
+import com.project.shopapp.responses.LoginResponse;
+import com.project.shopapp.responses.RegisterResponse;
+import com.project.shopapp.responses.ResponseObject;
+import com.project.shopapp.responses.UserResponse;
+import com.project.shopapp.services.IUserService;
+import com.project.shopapp.components.LocalizationUtils;
+import com.project.shopapp.services.TokenService;
+import com.project.shopapp.utils.MessageKeys;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import com.project.shopapp.dtos.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("${api.prefix}/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService userService;
+    private final TokenService tokenService;
+    private final LocalizationUtils localizationUtils;
+
+    @PostMapping("/register")
+    //can we register an "admin" user ?
+    public ResponseEntity<RegisterResponse> createUser(
+            @Valid @RequestBody UserDTO userDTO,
+            BindingResult result
+    ) {
+        RegisterResponse registerResponse = new RegisterResponse();
+
+        if (result.hasErrors()) {
+            List<String> errorMessages = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+
+            registerResponse.setMessage(errorMessages.toString());
+            return ResponseEntity.badRequest().body(registerResponse);
+        }
+
+        if (!userDTO.getPassword().equals(userDTO.getRetypePassword())) {
+            registerResponse.setMessage(localizationUtils.getLocalizedMessage(MessageKeys.PASSWORD_NOT_MATCH));
+            return ResponseEntity.badRequest().body(registerResponse);
+        }
+
+        try {
+            User user = userService.createUser(userDTO);
+            registerResponse.setMessage(localizationUtils.getLocalizedMessage(MessageKeys.REGISTER_SUCCESSFULLY));
+            registerResponse.setUser(user);
+            return ResponseEntity.ok(registerResponse);
+        } catch (Exception e) {
+            registerResponse.setMessage(e.getMessage());
+            return ResponseEntity.badRequest().body(registerResponse);
+        }
+    }
+
+    /*
+    Thêm tk admin
+    * */
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody UserLoginDTO userLoginDTO,
+            HttpServletRequest request
+    ) {
+        // Kiểm tra thông tin đăng nhập và sinh token
+        try {
+            String token = userService.login(
+                    userLoginDTO.getPhoneNumber(),
+                    userLoginDTO.getPassword()
+//                    userLoginDTO.getRoleId() == null ? 1 : userLoginDTO.getRoleId()
+            );
+            String userAgent = request.getHeader("User-Agent");
+            User userDetail = userService.getUserDetailsFromToken(token);
+            Token jwtToken = tokenService.addToken(userDetail,token, isMobileDevice(userAgent));
+            // Trả về token trong response
+            return ResponseEntity.ok(LoginResponse.builder()
+                            .message(localizationUtils.getLocalizedMessage(MessageKeys.LOGIN_SUCCESSFULLY))
+                            .token(token)
+                            .id(userDetail.getId())
+                            .fullname(userDetail.getFullName())
+                            .roles(userDetail.getAuthorities().stream().map(item -> item.getAuthority()).toList())
+                            .refreshToken(jwtToken.getRefreshToken())
+
+
+                    .build());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(
+                    LoginResponse.builder()
+                            .message(localizationUtils.getLocalizedMessage(MessageKeys.LOGIN_FAILED, e.getMessage()))
+                            .build()
+            );
+        }
+    }
+
+    private boolean isMobileDevice(String userAgent) {
+        // Kiểm tra User-Agent header để xác định thiết bị di động
+        // Ví dụ đơn giản:
+        return userAgent.toLowerCase().contains("mobile");
+    }
+
+    @PostMapping("/details")
+    public ResponseEntity<UserResponse> getUserDetails(
+            @RequestHeader("Authorization") String authorizationHeader
+    ) throws Exception {
+        String extractedToken = authorizationHeader.substring(7);
+        User user = userService.getUserDetailsFromToken(extractedToken);
+        return ResponseEntity.ok(UserResponse.fromUser(user));
+    }
+
+    @PutMapping("/details/{userId}")
+    public ResponseEntity<ResponseObject> updatUserDetails(
+            @PathVariable Long userId,
+            @RequestBody UpdateUserDTO updatedUserDTO,
+            @RequestHeader("Authorization") String authorizationHeader
+    )throws Exception {
+        String extractedToken = authorizationHeader.substring(7);
+        User user = userService.getUserDetailsFromToken(extractedToken);
+
+        //Kiem tra id
+        if (user.getId() != userId){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        User updatedUser = userService.updatedUser(userId, updatedUserDTO);
+        return ResponseEntity.ok().body(
+                ResponseObject.builder()
+                        .message("Update user detail successfully")
+                        .data(UserResponse.fromUser(updatedUser))
+                        .status(HttpStatus.OK)
+                        .build()
+        );
+
+    }
+
+    @PostMapping("/refreshToken")
+    public ResponseEntity<ResponseObject> refreshToken(
+            @Valid @RequestBody RefreshTokenDTO refreshTokenDTO
+    ) throws Exception {
+        User userDetail = userService.getUserDetailsFromToken(refreshTokenDTO.getRefreshToken());
+        Token jwtToken  = tokenService.refreshToken(refreshTokenDTO.getRefreshToken(), userDetail);
+        LoginResponse loginResponse = LoginResponse.builder()
+                .message("Refresh token successfully")
+                .token(jwtToken.getToken())
+                .tokenType(jwtToken.getTokenType())
+                .refreshToken(jwtToken.getRefreshToken())
+                .fullname(userDetail.getFullName())
+                .roles(userDetail.getAuthorities().stream().map(item -> item.getAuthority()).toList())
+                .id(userDetail.getId()).build();
+        return ResponseEntity.ok().body(
+                ResponseObject.builder()
+                        .data(loginResponse)
+                        .message(loginResponse.getMessage())
+                        .status(HttpStatus.OK)
+                        .build()
+        );
+    }
+
+
+}
---
+++ src/main/java/com/project/shopapp/services/IUserService.java
@@ -0,0 +1,18 @@
+package com.project.shopapp.services;
+
+import com.project.shopapp.dtos.UpdateUserDTO;
+import com.project.shopapp.dtos.UserDTO;
+import com.project.shopapp.exceptions.DataNotFoundException;
+import com.project.shopapp.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IUserService {
+    User createUser(UserDTO userDTO) throws Exception;
+//    String login(String phoneNumber, String password, Long roleId) throws Exception;
+String login(String phoneNumber, String password) throws Exception;
+    User getUserDetailsFromToken(String token) throws Exception;
+    User updatedUser(Long userId, UpdateUserDTO updatedUserDTO) throws Exception;
+    Page<User> fillAll(String keyword, Pageable pageable);
+
+}
